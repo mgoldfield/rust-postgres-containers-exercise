2597db94eaf8df5e889555a7fce93bdd58dc19a8
Matt <mgoldfield@gmail.com>
2023-05-09 02:45:48 +0200

    computed index for constant time min/max lookups

--- load-data/cpu_usage.sql
+++ load-data/cpu_usage.sql
@@ -5,13 +5,14 @@ DROP DATABASE IF EXISTS homework;
 CREATE DATABASE homework;
 \c homework
 
-CREATE EXTENSION IF NOT EXISTS timescaledb;
-
 CREATE TABLE IF NOT EXISTS cpu_usage(
   ts    TIMESTAMPTZ,
   host  TEXT,
   usage DOUBLE PRECISION
 );
+
+-- this index should give constant time lookups for min and max usage with host and truncated ts predicates
+CREATE INDEX ON cpu_usage USING (host, date_trunc('minute', ts), usage);
 SELECT create_hypertable('cpu_usage', 'ts', if_not_exists => true);
 
 CREATE TABLE IF NOT EXISTS cpu_stats_queries(
